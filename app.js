4dd2cfcb3792350b49da601dfc237c74b1679704
Saad <61311969+Saad5270@users.noreply.github.com>
2023-05-23 23:53:32 +0500

    incomplete responsive design

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function btnNav(){
+    // let respDiv= document.getElementById('respDiv');
+    // let boolResp= true;
+    // if(boolResp){
+    //     respDiv.style.display= 'flex';
+    //     boolResp= false;
+    // }
+    // else{
+    //     respDiv.style.display= 'none';
+    //     boolResp= true;
+
+
+    // }
+}
